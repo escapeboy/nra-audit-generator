2623eaf0c917c8f375a11fc427614175da7cd87f
Nikola Katsarov <katsarov@gmail.com>
2020-12-27 20:23:24 +0200

    fix order totals

--- lib/Order.php
+++ lib/Order.php
@@ -141,7 +141,7 @@ class Order
     {
         $total = 0;
         foreach($this->items as $item){
-            $total+=$item->getPrice();
+            $total+=$item->getPrice() * $item->getQuantity();
         }
 
         return $total;
@@ -151,7 +151,7 @@ class Order
     {
         $total = 0;
         foreach($this->items as $item){
-            $total+=$item->getVat();
+            $total+=$item->getVat() * $item->getQuantity();
         }
 
         return $total;
